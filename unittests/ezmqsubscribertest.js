23bfbca97cb0a13a1d97c0d9bb97eba1079cc77a
mj <myeong.jeong@samsung.com>
2018-12-03 11:11:31 +0900

    v1.0 commit
    Signed-off-by: mj <myeong.jeong@samsung.com>

---
+++ unittests/ezmqsubscribertest.js
@@ -0,0 +1,93 @@
+var assert = require('assert');
+var ezmq = require('../');
+var protoEvent = require('./ezmqtestutils.js');
+
+function subCallback(ezmqEvent) {}
+
+function subTopicCallback(topic, ezmqEvent) {}
+
+var ezmqAPI = new ezmq.EZMQAPI();
+ezmqAPI.initialize();
+var ip = 'localhost';
+var port = 5562;
+var ezmqSub = new ezmq.EZMQSubscriber(ip, port, subCallback, subTopicCallback);
+
+describe('#start()', function() {
+    it('should return EZMQ_OK', function() {
+        assert.equal(ezmqSub.start(), ezmq.EZMQErrorCode.EZMQ_OK);
+    });
+});
+
+describe('#subscribe()', function() {
+    it('should return EZMQ_OK', function() {
+        assert.equal(ezmqSub.subscribe(), ezmq.EZMQErrorCode.EZMQ_OK);
+    });
+});
+
+describe('#subscribeOnTopic()', function() {
+    it('should return EZMQ_OK', function() {
+        assert.equal(ezmqSub.subscribe('topic'), ezmq.EZMQErrorCode.EZMQ_OK);
+    });
+});
+
+describe('#subscribeOnTopicList()', function() {
+    it('should return EZMQ_OK', function() {
+        var topicList = ['Topic1', 'Topic2'];
+        assert.equal(ezmqSub.subscribe(topicList), ezmq.EZMQErrorCode.EZMQ_OK);
+    });
+});
+
+describe('#subscribeSecured()', function() {
+    it('should return EZMQ_OK', function() {
+        var clientSecretKey = "ZB1@RS6Kv^zucova$kH(!o>tZCQ.<!Q)6-0aWFmW";
+        var clientPublicKey = "-QW?Ved(f:<::3d5tJ$[4Er&]6#9yr=vha/caBc(";
+        var serverPublicKey = "tXJx&1^QE2g7WCXbF.$$TVP.wCtxwNhR8?iLi&S<";
+        try {
+            assert.equal(ezmqSub.setClientKeys(clientSecretKey, clientPublicKey), ezmq.EZMQErrorCode.EZMQ_OK);
+            assert.equal(ezmqSub.setServerPublicKey(serverPublicKey), ezmq.EZMQErrorCode.EZMQ_OK);
+        } catch (err) {}
+        assert.equal(ezmqSub.start(), ezmq.EZMQErrorCode.EZMQ_OK);
+        assert.equal(ezmqSub.subscribe(), ezmq.EZMQErrorCode.EZMQ_OK);
+        assert.equal(ezmqSub.subscribeWithIPPort(ip, port, "topic"), ezmq.EZMQErrorCode.EZMQ_OK);
+    });
+});
+
+describe('#unSubscribe()', function() {
+    it('should return EZMQ_OK', function() {
+        assert.equal(ezmqSub.unSubscribe(), ezmq.EZMQErrorCode.EZMQ_OK);
+    });
+});
+
+describe('#unSubscribeOnTopic()', function() {
+    it('should return EZMQ_OK', function() {
+        assert.equal(ezmqSub.unSubscribe('topic'), ezmq.EZMQErrorCode.EZMQ_OK);
+    });
+});
+
+describe('#unSubscribeOnTopicList()', function() {
+    it('should return EZMQ_OK', function() {
+        var topicList = ['Topic1', 'Topic2'];
+        assert.equal(
+            ezmqSub.unSubscribe(topicList),
+            ezmq.EZMQErrorCode.EZMQ_OK
+        );
+    });
+});
+
+describe('#getIp()', function() {
+    it('should return 5562', function() {
+        assert.equal(ezmqSub.getIp(), ip);
+    });
+});
+
+describe('#getPort()', function() {
+    it('should return 5562', function() {
+        assert.equal(ezmqSub.getPort(), port);
+    });
+});
+
+describe('#stop()', function() {
+    it('should return EZMQ_OK', function() {
+        assert.equal(ezmqSub.stop(), ezmq.EZMQErrorCode.EZMQ_OK);
+    });
+});
